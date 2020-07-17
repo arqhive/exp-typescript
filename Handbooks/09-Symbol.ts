a491c26a4fe0ebead3c84806bbcab79bb8abd813
arqhive <hgystation@gmail.com>
2020-07-17 18:42:33 +0900

    09. Symbol

---
+++ Handbooks/09-Symbol.ts
@@ -0,0 +1,24 @@
+// 기본 데이터 유형 Symbol
+
+// 생성자를 호출하여 값을 만든다.
+let sym1 = Symbol();
+let sym2 = Symbol("key");
+let sym3 = Symbol("key");
+
+sym2 === sym3; // false
+
+// 심볼을 객체 속성의 키로 사용할 수 있다.
+let obj = {
+  [sym1]: "value"
+};
+
+const getClassNameSymbol = Symbol();
+
+class C {
+  [getClassNameSymbol]() {
+    return "C";
+  }
+}
+
+let c = new C();
+let className = c[getClassNameSymbol](); // "C"
