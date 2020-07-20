d62991136b6f6b753bcade84e86bbbaa9730e06b
arqhive <hgystation@gmail.com>
2020-07-20 15:48:39 +0900

    05. Unions and Intersection types

---
+++ 05-Unions and Intersection Types.ts
@@ -0,0 +1,59 @@
+/* Union Type */
+function padLeft(value: string, padding: string | number) {
+  if (typeof padding === "number") {
+    return Array(padding + 1).join(" ") + value;
+  }
+  if (typeof padding === "string") {
+    return padding + value;
+  }
+  throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+padLeft("Hello world", 4);
+
+// Error
+let indentedString = padLeft("Hello world", true);
+
+// 유니언 타입은 모든 유형에 공통인 멤버에만 접근 가능
+interface Bird {
+  fly(): void;
+  layEggs(): void;
+}
+
+interface Fish {
+  swim(): void;
+  layEggs(): void;
+}
+
+declare function getSmallPet(): Fish | Bird;
+
+let pet = getSmallPet();
+pet.layEggs(); // Ok
+pet.swim(); // swim은 Fish에만 있음
+
+/* Intersection type */
+// 모든 유형을 하나로 합침
+interface ErrorHandling {
+  success: boolean;
+  error?: { message: string };
+}
+
+interface ArtworksData {
+  artworks: { title: string }[];
+}
+
+interface ArtistsData {
+  artists: { name: string }[];
+}
+
+type ArtworksResponse = ArtworksData & ErrorHandling;
+type ArtistsResponse = ArtistsData & ErrorHandling;
+
+const handleArtistsResponse = (response: ArtistsResponse) => {
+  if (response.error) {
+    console.error(response.error.message);
+    return;
+  }
+
+  console.log(response.artists);
+};
